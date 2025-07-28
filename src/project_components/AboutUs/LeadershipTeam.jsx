717dd50a2c6604ec07e172e56514cbd8aeb1f120
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-07-28 16:41:57 +0600

    about us

--- src/project_components/AboutUs/LeadershipTeam.jsx
+++ src/project_components/AboutUs/LeadershipTeam.jsx
@@ -3,76 +3,77 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
 
 function LeadershipTeam() {
-  const teamMembers = [
-    {
-      name: "Jane Doe",
-      title: "Chief Executive Officer",
-      src: "/AboutUs/leader_1.png",
-      alt: "leader_1",
-      description:
-        " A visionary leader and certified 8(a)/WOSB business owner, Jane has over 20 years of experience in federal contracting and workforce solutions. She leads strategic growth",
-    },
-    {
-      name: "John Smith",
-      title: "Chief Operating Officer",
-      src: "/AboutUs/leader_2.png",
-      alt: "leader_2",
-      description:
-        " John manages daily operations, delivery timelines, and large-scale client programs across multiple industries. His background in IT services, logistics, and contract fulfillment ensures smooth execution from intake to implementation.",
-    },
-    {
-      name: "Maria Chen",
-      title: "Chief Technology Officer",
-      src: "/AboutUs/leader_3.png",
-      alt: "leader_3",
-      description:
-        " John manages daily operations, delivery timelines, and large-scale client programs across multiple industries. His background in IT services, logistics, and contract fulfillment ensures smooth execution from intake to implementation.",
-    },
-    {
-      name: "Steve Mark",
-      title: "Chief Financial Officer",
-      src: "/About Us/leader_4.png",
-      alt: "leader_4",
-      description:
-        " John manages daily operations, delivery timelines, and large-scale client programs across multiple industries. His background in IT services, logistics, and contract fulfillment ensures smooth execution from intake to implementation.",
-    },
-  ];
+	const teamMembers = [
+		{
+			name: "Jane Doe",
+			title: "Chief Executive Officer",
+			src: "/AboutUs/leader_1.png",
+			alt: "leader_1",
+			description:
+				" A visionary leader and certified 8(a)/WOSB business owner, Jane has over 20 years of experience in federal contracting and workforce solutions. She leads strategic growth",
+		},
+		{
+			name: "John Smith",
+			title: "Chief Operating Officer",
+			src: "/AboutUs/leader_2.png",
+			alt: "leader_2",
+			description:
+				" John manages daily operations, delivery timelines, and large-scale client programs across multiple industries. His background in IT services, logistics, and contract fulfillment ensures smooth execution from intake to implementation.",
+		},
+		{
+			name: "Maria Chen",
+			title: "Chief Technology Officer",
+			src: "/AboutUs/leader_3.png",
+			alt: "leader_3",
+			description:
+				" John manages daily operations, delivery timelines, and large-scale client programs across multiple industries. His background in IT services, logistics, and contract fulfillment ensures smooth execution from intake to implementation.",
+		},
+		{
+			name: "Steve Mark",
+			title: "Chief Financial Officer",
+			src: "/AboutUs/leader_4.png",
+			alt: "leader_4",
+			description:
+				" John manages daily operations, delivery timelines, and large-scale client programs across multiple industries. His background in IT services, logistics, and contract fulfillment ensures smooth execution from intake to implementation.",
+		},
+	];
 
-  return (
-    <div className="py-12 px-4 sm:px-6 lg:px-8 bg-transparent mb-10">
-      <div className="max-w-[85%] mx-auto ">
-        <h2 className="gradient-h1 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center mb-12">
-          Leadership Team
-        </h2>
+	return (
+		<div className="py-12 px-4 sm:px-6 lg:px-8 bg-transparent mb-10">
+			<div className="max-w-[85%] mx-auto ">
+				<h2 className="text-gradient-heading text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center mb-12">
+					Leadership Team
+				</h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 md:px-4">
-          {teamMembers.map((member, index) => (
-            <Card
-              key={index}
-              className="group  h-full shadow-none border-none bg-transparent transition-shadow duration-300 p-0"
-            >
-              <CardHeader className="p-0">
-                <Image
-                  src={member.src}
-                  width={400}
-                  height={400}
-                  alt={member.alt}
-                  className="w-full group-hover:scale-105 ease-in duration-300 "
-                />
-              </CardHeader>
-              <CardContent className="text-center px-4">
-                <h3 className="text-xl md:text-2xl font-semibold text-white">
-                  {member.name}
-                </h3>
-                <p className="text-sm text-white">{member.title}</p>
-                <p className="text-white pt-5">{member.description}</p>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 md:px-4">
+					{teamMembers.map((member, index) => (
+						<Card
+							key={index}
+							className="group  h-full shadow-none border-none bg-transparent transition-shadow duration-300 p-0">
+							<CardHeader className="p-0">
+								<Image
+									src={member.src}
+									width={400}
+									height={400}
+									alt={member.alt}
+									className="w-full group-hover:scale-105 ease-in duration-300 "
+								/>
+							</CardHeader>
+							<CardContent className="text-center px-4">
+								<h3 className="text-xl md:text-2xl font-semibold text-gradient-heading">
+									{member.name}
+								</h3>
+								<p className="text-sm font-semibold text-gray-700">
+									{member.title}
+								</p>
+								<p className="text-gray-700 pt-5">{member.description}</p>
+							</CardContent>
+						</Card>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default LeadershipTeam;
--- src/project_components/footer/Footer.jsx
+++ src/project_components/footer/Footer.jsx
@@ -4,143 +4,135 @@ import Link from "next/link";
 import provideIcon from "@/IconProvider/IconProvider";
 
 export function Footer() {
-  return (
-    <div className="bg-transparent footer-glass text-white dark:text-gray-300">
-      <div className=" mx-auto px-4 py-12">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {/* Company Logo and Quick Links */}
-          <div className="space-y-4">
-            <h1 className="text-2xl font-bold text-white dark:text-white">
-              Company Logo
-            </h1>
-          </div>
-          <div className="space-y-4">
-            <h3 className="text-lg font-semibold text-white dark:text-gray-200">
-              Quick Links
-            </h3>
-            <div className="flex gap-4">
-              <div>
-                <ul className="space-y-1">
-                  <li>
-                    <Link
-                      href="/product-provisioning-services"
-                      className="hover:text-white/70 transition-colors"
-                    >
-                      Service
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="/about-us"
-                      className="hover:text-white/70 transition-colors"
-                    >
-                      About Us
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="/contact-us"
-                      className="hover:text-white/70 transition-colors"
-                    >
-                      Contact Us
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <ul className="space-y-1">
-                  <li>
-                    <Link
-                      href="/blog"
-                      className="hover:text-white/70 transition-colors"
-                    >
-                      Blog
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="/industrial-serve"
-                      className="hover:text-white/70 transition-colors"
-                    >
-                      Industrial Serve
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="#"
-                      className="hover:text-white/70 transition-colors"
-                    >
-                      Featured Clients
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+	return (
+		<div className="gardient-style text-white dark:text-gray-300">
+			<div className=" mx-auto px-4 py-12">
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+					{/* Company Logo and Quick Links */}
+					<div className="space-y-4">
+						<h1 className="text-2xl ml-4 font-bold text-white dark:text-white">
+							{provideIcon({ name: "company_logo_shield" })}
+						</h1>
+					</div>
+					<div className="space-y-4">
+						<h3 className="text-lg font-semibold text-white dark:text-gray-200">
+							Quick Links
+						</h3>
+						<div className="flex gap-4">
+							<div>
+								<ul className="space-y-1">
+									<li>
+										<Link
+											href="/product-provisioning-services"
+											className="hover:text-white/70 transition-colors">
+											Service
+										</Link>
+									</li>
+									<li>
+										<Link
+											href="/about-us"
+											className="hover:text-white/70 transition-colors">
+											About Us
+										</Link>
+									</li>
+									<li>
+										<Link
+											href="/contact-us"
+											className="hover:text-white/70 transition-colors">
+											Contact Us
+										</Link>
+									</li>
+								</ul>
+							</div>
+							<div>
+								<ul className="space-y-1">
+									<li>
+										<Link
+											href="/blog"
+											className="hover:text-white/70 transition-colors">
+											Blog
+										</Link>
+									</li>
+									<li>
+										<Link
+											href="/industrial-serve"
+											className="hover:text-white/70 transition-colors">
+											Industrial Serve
+										</Link>
+									</li>
+									<li>
+										<Link
+											href="#"
+											className="hover:text-white/70 transition-colors">
+											Featured Clients
+										</Link>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
 
-          {/* Mobile App */}
-          <div className="space-y-4">
-            <h3 className="text-lg font-semibold text-white dark:text-gray-200 ">
-              Our Contact
-            </h3>
-            <div className="flex flex-col space-y-3">
-              <p className="leading-6">
-                Email: <br />
-                <span className="font-bold">example@gmail.com</span>
-              </p>
+					{/* Mobile App */}
+					<div className="space-y-4">
+						<h3 className="text-lg font-semibold text-white dark:text-gray-200 ">
+							Our Contact
+						</h3>
+						<div className="flex flex-col space-y-3">
+							<p className="leading-6">
+								Email: <br />
+								<span className="font-bold">example@gmail.com</span>
+							</p>
 
-              <p className="leading-6">
-                LinkedIn <br />
-                <span className="font-bold">examplelinkedin.com</span>
-              </p>
-            </div>
-          </div>
+							<p className="leading-6">
+								LinkedIn <br />
+								<span className="font-bold">examplelinkedin.com</span>
+							</p>
+						</div>
+					</div>
 
-          {/* Newsletter */}
-          <div className="space-y-4">
-            <div>
-              <h3 className="text-3xl font-bold text-white dark:text-gray-200">
-                Join Our Community
-              </h3>
-              <p className="text-sm mt-1">
-                Receive pricing updates, shopping tips & more!
-              </p>
-            </div>
+					{/* Newsletter */}
+					<div className="space-y-4">
+						<div>
+							<h3 className="text-3xl font-bold text-white dark:text-gray-200">
+								Join Our Community
+							</h3>
+							<p className="text-sm mt-1">
+								Receive pricing updates, shopping tips & more!
+							</p>
+						</div>
 
-            <div className="flex flex-col space-y-2 max-w-full">
-              <div className="footer-newsletter-gradient p-4 max-w-4/5 rounded-full flex gap-4">
-                <Input
-                  type="email"
-                  placeholder="Your email"
-                  className="border-none focus:border-none shadow-none max-w-64 placeholder:text-blue-950"
-                />
-                <Button className="footer-signup-btn-gradient max-w-20 rounded-full">
-                  Sign Up
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
+						<div className="flex flex-col space-y-2 max-w-full">
+							<div className="footer-newsletter-gradient p-4 max-w-4/5 rounded-full flex gap-4">
+								<Input
+									type="email"
+									placeholder="Your email"
+									className="border-none focus:border-none shadow-none max-w-64 placeholder:text-blue-950"
+								/>
+								<Button className="gardient-style max-w-20 rounded-full">
+									Sign Up
+								</Button>
+							</div>
+						</div>
+					</div>
+				</div>
 
-        {/* Bottom section */}
-        <div className="border-t border-gray-200 dark:border-gray-700 mt-12 pt-8 flex flex-col md:flex-row justify-between items-center gap-4">
-          <p className="text-sm">© 2024 example.com. All rights reserved.</p>
-          <div className="flex gap-4">
-            <a
-              href="#"
-              className="text-sm hover:text-primary transition-colors"
-            >
-              Terms & Conditions
-            </a>
-            <a
-              href="#"
-              className="text-sm hover:text-primary transition-colors"
-            >
-              Privacy Notice
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+				{/* Bottom section */}
+				<div className="border-t border-gray-200 dark:border-gray-700 mt-12 pt-8 flex flex-col md:flex-row justify-between items-center gap-4">
+					<p className="text-sm">© 2024 example.com. All rights reserved.</p>
+					<div className="flex gap-4">
+						<a
+							href="#"
+							className="text-sm hover:text-primary transition-colors">
+							Terms & Conditions
+						</a>
+						<a
+							href="#"
+							className="text-sm hover:text-primary transition-colors">
+							Privacy Notice
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
