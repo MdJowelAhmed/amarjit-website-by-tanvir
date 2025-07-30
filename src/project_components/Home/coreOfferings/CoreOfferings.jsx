a5cfdc4aa0537e61dccb18d5ca8053b085e3e3e6
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-07-30 15:11:16 +0600

    shiiper on home page

--- src/project_components/Home/coreOfferings/CoreOfferings.jsx
+++ src/project_components/Home/coreOfferings/CoreOfferings.jsx
@@ -66,6 +66,52 @@ const CoreOfferings = () => {
 		},
 	];
 
+	const shippers = [
+		{
+			id: 1,
+			image: "/Shipper/shipper_1.jpg",
+			title: "Full Truck Load",
+			description:
+				"Access to a large network of carriers for high-volume shipments.",
+			alt: "Full Truck Load",
+		},
+		{
+			id: 2,
+			image: "/Shipper/shipper_2.jpg",
+			title: "Climate Control",
+			description: "Specialized transport for temperature-sensitive goods.",
+			alt: "Climate Control",
+		},
+		{
+			id: 3,
+			image: "/Shipper/shipper_3.png",
+			title: "Flatbed Trucking",
+			description: "Solutions for oversized or irregular shipments.",
+			alt: "Flatbed Trucking",
+		},
+
+		{
+			id: 4,
+			image: "/Shipper/shipper_4.jpg",
+			title: "LTL FREIGHT",
+			description: "Cost-effective shipping for smaller freight.",
+			alt: "LTL FREIGHT",
+		},
+		{
+			id: 5,
+			image: "/Shipper/shipper_5.jpg",
+			title: "Cross Border Logistics",
+			description: "Handling US-Canada shipments with customs coordination.",
+			alt: "Cross Border Logistics",
+		},
+		{
+			id: 6,
+			image: "/Shipper/shipper_6.png",
+			title: "Drayage",
+			description: "Managing container deliveries and returns efficiently.",
+			alt: "Drayage",
+		},
+	];
 	function StaffingSection() {
 		return (
 			<section>
@@ -182,6 +228,57 @@ const CoreOfferings = () => {
 										</CardContent>
 									</Card>
 								</Link>
+							))}
+						</div>
+					</div>
+				</div>
+			</section>
+			<section className="bg-transparent  py-16 sm:py-20 lg:py-12  overflow-hidden">
+				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
+					{/* Header Section */}
+
+					{/* Staffing Solutions Section */}
+					<div className="mb-16 sm:mb-20">
+						<div className="mb-10">
+							<h1 className="text-2xl sm:text-3xl leading-relaxed lg:text-4xl xl:text-5xl font-bold mb-3 sm:mb-6 text-gradient-heading text-center ">
+								Shipper
+							</h1>
+							<p className="text-base  sm:text-lg text-[#181818]  mx-auto w-full text-center">
+								We provide tailored logistics solutions for shippers seeking
+								reliability, visibility, and cost-efficiency. Whether you're
+								moving full truckloads or managing multi-point deliveries, our
+								platform connects you with a trusted carrier network, real-time
+								tracking, and flexible freight options.{" "}
+							</p>
+						</div>
+
+						{/* Staffing Cards Grid */}
+						<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8">
+							{shippers.map((item) => (
+								<div key={item.id}>
+									<Card className=" h-[30rem] group hover:shadow-xl transition-all duration-300 relative overflow-hidden">
+										{/* Solid color overlay that grows from top on hover */}
+										<div className="absolute left-0 top-0 w-full h-0 group-hover:h-full bg-[#bbbbfa] transition-all duration-500 z-10 pointer-events-none"></div>
+										<CardHeader className="mt-5 text-center relative z-20">
+											<CardTitle className=" text-xl sm:text-2xl font-bold text-[#181818] mb-3 group-hover:text-[#181818] transition-colors">
+												{item.title}
+											</CardTitle>
+											<CardDescription className=" max-w-60 mx-auto text-[#181818] text-sm sm:text-base leading-relaxed min-w-[85%]">
+												{item.description}
+											</CardDescription>
+										</CardHeader>
+										<CardContent className="p-6 relative z-20">
+											<div className="relative h-48 sm:h-56 lg:h-64 overflow-hidden rounded-lg ">
+												<Image
+													src={item.image}
+													alt={item.alt}
+													fill
+													className="object-cover rounded-lg group-hover:scale-105 transition-transform duration-300"
+												/>
+											</div>
+										</CardContent>
+									</Card>
+								</div>
 							))}
 						</div>
 					</div>
