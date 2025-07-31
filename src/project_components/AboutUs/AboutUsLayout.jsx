14e2d73a880871180a395c461a613edb94b23b00
MdJowelAhmed <mdjowelahmed924@gmail.com>
2025-07-31 11:44:40 +0600

    update conflick

--- src/project_components/AboutUs/AboutUsLayout.jsx
+++ src/project_components/AboutUs/AboutUsLayout.jsx
@@ -5,22 +5,18 @@ import CoreValue from "./CoreValue";
 import LeadershipTeam from "./LeadershipTeam";
 
 function AboutUsLayout() {
-	const setAboutUsBanner = {
-		src: "/About Us/About_Us.png",
-		heading: "About Us",
-	};
-	// console.log("setAboutUsBanner");
-	// console.log(setAboutUsBanner.src);
-	// console.log(setAboutUsBanner);
-	// console.log(setAboutUsBanner.heading);
-	return (
-		<div>
-			<Banner src={setAboutUsBanner.src} heading={setAboutUsBanner.heading} />
-			<OurMission />
-			<CoreValue />
-			<LeadershipTeam />
-		</div>
-	);
+  const setAboutUsBanner = {
+    src: "/AboutUs/AboutUs.jpg",
+    heading: "About Us",
+  };
+  return (
+    <div>
+      <Banner src={setAboutUsBanner.src} heading={setAboutUsBanner.heading} />
+      <OurMission />
+      <CoreValue />
+      <LeadershipTeam />
+    </div>
+  );
 }
 
 export default AboutUsLayout;
