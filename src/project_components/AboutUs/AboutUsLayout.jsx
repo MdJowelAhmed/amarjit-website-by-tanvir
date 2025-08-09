cadc1632819cd43ec4cde764749b430fb5a01569
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-08-09 11:07:07 +0600

    added 2 images and fixed small issues

--- src/project_components/AboutUs/AboutUsLayout.jsx
+++ src/project_components/AboutUs/AboutUsLayout.jsx
@@ -18,21 +18,21 @@ function AboutUsLayout() {
 					src={"/left_img.png"}
 					width={500}
 					height={500}
-					className="absolute h-full md:h-[450px] 2xl:h-[550px] w-full md:w-[420px] 2xl:w-[550px] md:top-[12rem] 2xl:top-[14rem] md:left-40 2xl:left-48 scale-170"
+					className="hidden lg:block absolute h-full md:h-[450px] 2xl:h-[550px] w-full md:w-[420px] 2xl:w-[550px] md:top-[12rem] 2xl:top-[14rem] md:left-40 2xl:left-48 scale-170"
 					alt="bg-image"
 				/>
 				<Image
 					src={"/right_img.png"}
 					width={500}
 					height={500}
-					className="absolute h-full md:h-[450px] 2xl:h-[550px] w-full md:w-[420px] 2xl:w-[550px] md:top-[62rem] 2xl:top-[65rem] md:right-0 2xl:right-20 scale-170"
+					className="hidden lg:block absolute h-full md:h-[450px] 2xl:h-[550px] w-full md:w-[420px] 2xl:w-[550px] md:top-[62rem] 2xl:top-[65rem] md:right-0 2xl:right-20 scale-170"
 					alt="bg-image"
 				/>
 				<Image
 					src={"/left_img.png"}
 					width={500}
 					height={500}
-					className="absolute h-full md:h-[450px] 2xl:h-[550px] w-full md:w-[420px] 2xl:w-[550px] md:bottom-[24rem] 2xl:bottom-[20rem] md:left-0 2xl:left-48 scale-170 -z-10"
+					className="hidden lg:block absolute h-full md:h-[450px] 2xl:h-[550px] w-full md:w-[420px] 2xl:w-[550px] md:bottom-[24rem] 2xl:bottom-[20rem] md:left-0 2xl:left-48 scale-170 -z-10"
 					alt="bg-image"
 				/>
 
