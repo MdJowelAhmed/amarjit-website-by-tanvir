be9c1ca0360214888da86d8c2a4abbbd64a89791
TanvirAhmed2942 <tanvirahmed.3264@gmail.com>
2025-08-11 09:42:24 +0600

    certiification section

--- src/project_components/Certification/GSATable.jsx
+++ src/project_components/Certification/GSATable.jsx
@@ -1,130 +1,213 @@
 import HeadingSection from "@/utils/provideHeadingSubheading";
 
 function NAICSTable() {
-	function naicsHeading() {
-		return (
-			<section>
-				<HeadingSection
-					heading="NAICS Codes"
-					subheading="We maintain all required business identifiers to ensure full alignment with federal and commercial procurement standards."
-				/>
-			</section>
-		);
-	}
-	const naicsData = {
-		staffingTalent: [
-			{ code: "561320", description: "Temporary Help Services" },
-			{ code: "561311", description: "Employment Placement Agencies" },
-			{ code: "541612", description: "Human Resources Consulting Services" },
-		],
-		procurementProducts: [
-			{
-				code: "423840",
-				description: "Industrial Supplies Merchant Wholesalers",
-			},
-			{
-				code: "423430",
-				description:
-					"Computer and Computer Peripheral Equipment and Software Wholesalers",
-			},
-			{
-				code: "423490",
-				description:
-					"Other Professional Equipment and Supplies Merchant Wholesalers",
-			},
-		],
-		technologySolutions: [
-			{ code: "541511", description: "Custom Computer Programming Services" },
-			{ code: "541512", description: "Computer Systems Design Services" },
-			{ code: "541519", description: "Other Computer Related Services" },
-		],
-		logisticsSupplyChain: [
-			{ code: "488510", description: "Freight Transportation Arrangement" },
-			{
-				code: "541614",
-				description:
-					"Process, Physical Distribution, and Logistics Consulting Services",
-			},
-			{ code: "492110", description: "Couriers and Express Delivery Services" },
-			{ code: "493110", description: "General Warehousing and Storage" },
-		],
-		governmentSupport: [
-			{ code: "561110", description: "Office Administrative Services" },
-			{
-				code: "541611",
-				description:
-					"Administrative Management and General Management Consulting Services",
-			},
-		],
-	};
+  function naicsHeading() {
+    return (
+      <section>
+        <HeadingSection
+          heading="NAICS Codes"
+          subheading="We maintain all required business identifiers to ensure full alignment with federal and commercial procurement standards."
+        />
+      </section>
+    );
+  }
+  const naicsData = {
+    staffingTalent: [
+      { code: "561311", description: "Employment Placement Agencies" },
+      { code: "561312", description: "Executive Search Services" },
+      { code: "561320", description: "Temporary Help Services" },
+      { code: "541612", description: "Human Resources Consulting Services" },
+    ],
+    procurementProducts: [
+      {
+        code: "337214",
+        description: "Office Furniture (Except Wood) Manufacturing",
+      },
+      {
+        code: "423450",
+        description:
+          "Medical, Dental, and Hospital Equipment and Supplies Merchant Wholesalers",
+      },
+      {
+        code: "423430",
+        description:
+          "Computer and Computer Peripheral Equipment and Software Wholesalers",
+      },
+      {
+        code: "423490",
+        description:
+          "Other Professional Equipment and Supplies Merchant Wholesalers",
+      },
+      {
+        code: "423840",
+        description: "Industrial Supplies Merchant Wholesalers",
+      },
+      {
+        code: "424120",
+        description: "Stationery and Office Supplies Merchant Wholesalers",
+      },
 
-	const CategoryTable = ({ title, data }) => (
-		<div className="mb-10 border border-[#4f3075]">
-			{/* Category Header */}
-			<div className=" border-b border-[#4f3075]">
-				<h3 className="text-center font-semibold py-4 px-4 text-gray-800 text-lg">
-					{title}
-				</h3>
-			</div>
+      {
+        code: "621399",
+        description: "Offices of All Other Miscellaneous Health Practitioners",
+      },
+    ],
+    technologySolutions: [
+      { code: "541511", description: "Custom Computer Programming Services" },
+      { code: "541512", description: "Computer Systems Design Services" },
+      {
+        code: "541513",
+        description: "Computer Facilities Management Services",
+      },
+      { code: "541519", description: "Other Computer Related Services" },
+    ],
+    logisticsSupplyChain: [
+      { code: "484210", description: "Used Household and Office Goods Moving" },
+      {
+        code: "488510",
+        description: "Freight Transportation Arrangement",
+      },
+      { code: "492110", description: "Couriers and Express Delivery Services" },
+      { code: "493110", description: "General Warehousing and Storage" },
+      {
+        code: "541614",
+        description:
+          "Process, Physical Distribution, and Logistics Consulting Services",
+      },
+      {
+        code: "722320",
+        description: "Caterers",
+      },
+    ],
+    governmentSupport: [
+      {
+        code: "236220",
+        description: "Commercial and Institutional Building Construction",
+      },
+      {
+        code: "238220",
+        description: "Plumbing, Heating, and Air-Conditioning Contractors",
+      },
+      {
+        code: "238290",
+        description: "Other Building Equipment Contractors",
+      },
+      {
+        code: "238320",
+        description: "Painting and Wall Covering Contractors",
+      },
+      {
+        code: "541330",
+        description: "Engineering Services",
+      },
+      {
+        code: "541611",
+        description:
+          "Administrative Management and General Management Consulting Services",
+      },
+      {
+        code: "561110",
+        description: "Office Administrative Services",
+      },
+      {
+        code: "561210",
+        description: "Facilities Support Services",
+      },
+      {
+        code: "561720",
+        description: "Janitorial Services",
+      },
+      {
+        code: "561730",
+        description: "Landscaping Services",
+      },
+      {
+        code: "561920",
+        description: "Convention and Trade Show Organizers",
+      },
+      {
+        code: "611699",
+        description: "All Other Miscellaneous Schools and Instruction",
+      },
+      {
+        code: "611710",
+        description: "Educational Support Services",
+      },
+      {
+        code: "812199",
+        description: "Other Personal Care Services",
+      },
+    ],
+  };
 
-			{/* Column Headers */}
-			<div className="grid grid-cols-2 bg-gray-50 border-b border-[#4f3075]">
-				<div className="border-r border-[#4f3075] text-center font-semibold text-gray-800 py-3 px-4">
-					Code Type
-				</div>
-				<div className="text-center font-semibold text-gray-800 py-3 px-4">
-					Value
-				</div>
-			</div>
+  const CategoryTable = ({ title, data }) => (
+    <div className="mb-10 border border-[#4f3075]">
+      {/* Category Header */}
+      <div className=" border-b border-[#4f3075]">
+        <h3 className="text-center font-semibold py-4 px-4 text-gray-800 text-lg">
+          {title}
+        </h3>
+      </div>
 
-			{/* Data Rows */}
-			{data.map((item, index) => (
-				<div
-					key={index}
-					className="grid grid-cols-2 border-b border-[#4f3075] hover:bg-gray-50">
-					<div className="border-r border-[#4f3075] text-center py-4 px-4 font-medium">
-						{item.code}
-					</div>
-					<div className="text-center py-4 px-4">{item.description}</div>
-				</div>
-			))}
-		</div>
-	);
+      {/* Column Headers */}
+      <div className="grid grid-cols-2 bg-gray-50 border-b border-[#4f3075]">
+        <div className="border-r border-[#4f3075] text-center font-semibold text-gray-800 py-3 px-4">
+          Code Type
+        </div>
+        <div className="text-center font-semibold text-gray-800 py-3 px-4">
+          Value
+        </div>
+      </div>
 
-	return (
-		<div className="max-w-7xl mx-auto py-10 px-4 sm:px-6 lg:px-8 bg-white">
-			{/* Header Section */}
-			<div className="text-center mb-10">{naicsHeading()}</div>
+      {/* Data Rows */}
+      {data.map((item, index) => (
+        <div
+          key={index}
+          className="grid grid-cols-2 border-b border-[#4f3075] hover:bg-gray-50"
+        >
+          <div className="border-r border-[#4f3075] text-center py-4 px-4 font-medium">
+            {item.code}
+          </div>
+          <div className="text-center py-4 px-4">{item.description}</div>
+        </div>
+      ))}
+    </div>
+  );
 
-			{/* Tables */}
-			<div className="space-y-6">
-				<CategoryTable
-					title="Staffing & Talent Services"
-					data={naicsData.staffingTalent}
-				/>
+  return (
+    <div className="max-w-7xl mx-auto py-10 px-4 sm:px-6 lg:px-8 bg-white">
+      {/* Header Section */}
+      <div className="text-center mb-10">{naicsHeading()}</div>
 
-				<CategoryTable
-					title="Procurement & Essential Products"
-					data={naicsData.procurementProducts}
-				/>
+      {/* Tables */}
+      <div className="space-y-6">
+        <CategoryTable
+          title="Staffing & Talent Services"
+          data={naicsData.staffingTalent}
+        />
 
-				<CategoryTable
-					title="Technology Solutions & Services"
-					data={naicsData.technologySolutions}
-				/>
+        <CategoryTable
+          title="Procurement & Essential Products"
+          data={naicsData.procurementProducts}
+        />
 
-				<CategoryTable
-					title="Logistics & Supply Chain Services"
-					data={naicsData.logisticsSupplyChain}
-				/>
+        <CategoryTable
+          title="Technology Solutions & Services"
+          data={naicsData.technologySolutions}
+        />
 
-				<CategoryTable
-					title="Government Contracting Support (General)"
-					data={naicsData.governmentSupport}
-				/>
-			</div>
-		</div>
-	);
+        <CategoryTable
+          title="Logistics & Supply Chain Services"
+          data={naicsData.logisticsSupplyChain}
+        />
+
+        <CategoryTable
+          title="Government Contracting Support (General)"
+          data={naicsData.governmentSupport}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default NAICSTable;
--- src/project_components/Certification/SocioEconomic.jsx
+++ src/project_components/Certification/SocioEconomic.jsx
@@ -5,92 +5,93 @@ import HeadingSection from "@/utils/provideHeadingSubheading";
 import React, { useState } from "react";
 import Image from "next/image";
 import {
-	Dialog,
-	DialogClose,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 
 function SocioEconomic() {
-	const [selectedCertificate, setSelectedCertificate] = useState(null);
+  const [selectedCertificate, setSelectedCertificate] = useState(null);
 
-	function socioEconomicHeading() {
-		return (
-			<section>
-				<HeadingSection
-					heading="Socio-Economic Certifications"
-					subheading="As a certified small and minority-owned business, we bring diversity value to your contracts while meeting key socio-economic goals."
-				/>
-			</section>
-		);
-	}
+  function socioEconomicHeading() {
+    return (
+      <section>
+        <HeadingSection
+          heading="Socio-Economic Certifications"
+          subheading="As a certified small and minority-owned business, we bring diversity value to your contracts while meeting key socio-economic goals."
+        />
+      </section>
+    );
+  }
 
-	const socioEconomic = [
-		{
-			id: 1,
-			icon: provideIcon({ name: "certificate" }),
-			src: "/Certificate/cartificate.png",
-			headline: "MBE",
-			subheadLine: "Minority Business Enterprise",
-		},
-		{
-			id: 2,
-			icon: provideIcon({ name: "certificate" }),
-			src: "/Certificate/cartificate.png",
-			headline: "WOSB",
-			subheadLine: "Women-Owned Small Business",
-		},
-		{
-			id: 3,
-			icon: provideIcon({ name: "certificate" }),
-			src: "/Certificate/cartificate.png",
-			headline: "8(a) Certification ",
-			subheadLine: " SBA Certified",
-		},
-	];
+  const socioEconomic = [
+    {
+      id: 1,
+      icon: provideIcon({ name: "certificate" }),
+      src: "/Certificate/cartificate.png",
+      headline: "MBE",
+      subheadLine: "Minority Business Enterprise",
+    },
+    // {
+    // 	id: 2,
+    // 	icon: provideIcon({ name: "certificate" }),
+    // 	src: "/Certificate/cartificate.png",
+    // 	headline: "WOSB",
+    // 	subheadLine: "Women-Owned Small Business",
+    // },
+    {
+      id: 2,
+      icon: provideIcon({ name: "certificate" }),
+      src: "/Certificate/cartificate.png",
+      headline: "8(a) Certification ",
+      subheadLine: " SBA Certified",
+    },
+  ];
 
-	return (
-		<div className="pb-24">
-			{socioEconomicHeading()}
-			<div className="container mx-auto grid lg:grid-cols-3 gap-10">
-				{socioEconomic.map((item) => (
-					<Card key={item.id} className=" text-black flex items-center gap-1 ">
-						<Dialog>
-							<DialogTrigger
-								asChild
-								className="md:max-w-36 cursor-pointer hover:opacity-80 transition-opacity z-10"
-								onClick={() => setSelectedCertificate(item)}>
-								{provideIcon({ name: "certificate" })}
-							</DialogTrigger>
-							<DialogContent className=" max-w-4xl lg:min-w-4xl">
-								<DialogHeader>
-									<DialogTitle>{item.headline}</DialogTitle>
-									<DialogDescription>{item.subheadLine}</DialogDescription>
-								</DialogHeader>
-								<div className="flex justify-center">
-									<Image
-										src={item.src}
-										alt={`${item.headline} Certificate`}
-										width={800}
-										height={600}
-										className="max-w-full h-auto border rounded-lg shadow-lg"
-									/>
-								</div>
-							</DialogContent>
-						</Dialog>
-						<div className="flex flex-col items-center gap-2">
-							<h1 className="text-3xl font-bold">{item.headline}</h1>
-							<p className="text-lg font-medium">{item.subheadLine}</p>
-						</div>
-					</Card>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="pb-24">
+      {socioEconomicHeading()}
+      <div className="max-w-7xl px-4 md:px-6 lg:px-8 2xl:px-8 mx-auto grid lg:grid-cols-2 gap-10">
+        {socioEconomic.map((item) => (
+          <Card key={item.id} className=" text-black flex items-center gap-1 ">
+            <Dialog>
+              <DialogTrigger
+                asChild
+                className="md:max-w-36 cursor-pointer hover:opacity-80 transition-opacity z-10"
+                onClick={() => setSelectedCertificate(item)}
+              >
+                {provideIcon({ name: "certificate" })}
+              </DialogTrigger>
+              <DialogContent className=" max-w-4xl lg:min-w-4xl">
+                <DialogHeader>
+                  <DialogTitle>{item.headline}</DialogTitle>
+                  <DialogDescription>{item.subheadLine}</DialogDescription>
+                </DialogHeader>
+                <div className="flex justify-center">
+                  <Image
+                    src={item.src}
+                    alt={`${item.headline} Certificate`}
+                    width={800}
+                    height={600}
+                    className="max-w-full h-auto border rounded-lg shadow-lg"
+                  />
+                </div>
+              </DialogContent>
+            </Dialog>
+            <div className="flex flex-col items-center gap-2">
+              <h1 className="text-3xl font-bold">{item.headline}</h1>
+              <p className="text-lg font-medium">{item.subheadLine}</p>
+            </div>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default SocioEconomic;
